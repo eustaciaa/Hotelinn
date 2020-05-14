b7ebda8bc8ece94b1ba311c20b71828f4aed95b4
ando-hunterz <ando.hunterz@gmail.com>
2020-05-14 18:51:40 +0700

    migration user, hotel

--- database/migrations/2020_05_14_114637_create_hotel.php
+++ database/migrations/2020_05_14_114637_create_hotel.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateHotel extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('hotel', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('firstName');
-            $table->string('lastName');
-            $table->string('password');
+            $table->string('name');
+            $table->integer('rate');
             $table->string('photo');
-            $table->date('tgl_lahir');
-            $table->timestamps();
+            $table->string('review');
         });
     }
 
@@ -33,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('hotel');
     }
 }
