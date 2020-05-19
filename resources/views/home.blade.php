d5fbde5ee840cc1e7322ed5a25a321f1cb71db9a
eustaciaa <clara3@student.umn.ac.id>
2020-05-19 20:25:15 +0700

    ganti ganti warna, ubah tampilan menu filtering

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,50 +27,37 @@
         </div>
     </div>
 </div>
-<div class="container">
-    <div class="row justify-content-center" id="searchRow">
-        <div class="col-md-8">
-
-            <div class="card my-5 content-wrapper">
-                <div class="card-header">Search For Hotel</div>
-
-                <div class="card-body">
-
-                    <div class="form-group row">
-                        <label for="provinsi" class="col-md-4 col-form-label text-md-right">Provinsi</label>
-
-                        <div class="col-md-6">
+<div class="bg-lightblue">
+    <div class="container">
+        <div class="row justify-content-center" id="searchRow">
+            <div class="col-md-8 my-5">
+                <form>
+                    <div class="row justify-content-center">
+                        <div class="col">
                             <select class="form-control" name="provinsiId" id="provinsi">
-                                <option value="all" selected> Pilih... </option>
+                                <option value="all" selected> Pilih provinsi </option>
                                 @foreach ($provinsis as $provinsi)
                                 <option value="{{ $provinsi->id }}"> {{ $provinsi->namaProvinsi }} </option>
                                 @endforeach
                             </select>
                         </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="kota" class="col-md-4 col-form-label text-md-right">Kota</label>
-
-                        <div class="col-md-6">
+                        <div class="col">
                             <select class="form-control" name="kotaId" id="kota">
-                                <option value="null"> Pilih.. </option>
+                                <option value="null"> Pilih kota </option>
                             </select>
                         </div>
-                    </div>
-
-                    <div class="form-group row mb-0">
-                        <div class="col-md-6 offset-md-4">
+                        <div class="col">
                             <button type="submit" class="btn btn-primary" id="search">
                                 Search
                             </button>
                         </div>
                     </div>
-
-                </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
+<div class="container">
 
     <div class='row justify-content-center' >
         <div class='col-md-8' id='hotel-row'>
