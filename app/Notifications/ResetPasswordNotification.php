ca10cd50889f855b13415a34abd95bb860ae12cb
ando-hunterz <ando.hunterz@gmail.com>
2020-06-02 12:43:16 +0700

    mail

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    private $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+
+        $link = url( "/password/reset/?token=" . $this->token );
+
+        return (new MailMessage)
+                    ->subject('Memulihkan Password')
+                    ->greeting('Halo')
+                    ->line('Kamu menerima email ini karena kami menerima permintaan kamu untuk memulihkan password')
+                    ->action('Notification Action', $link)
+                    ->line('Link ini akan kadarluwarsa dalam 60 menit')
+                    ->line('Abaikan jika kamu tidak meminta untuk memulihkan password')
+                    ->line('Salam')
+                    ->line('Hotelin');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new Notifications\ResetPasswordNotification($token));
+    }
+
 }
